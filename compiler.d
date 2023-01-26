2bb1f3bb6b97d1d695112952242422da7742ace3
pyxiion <me@pyxiion.ru>
2023-01-26 22:50:05 +0300

    Add directives and instructions, saving to file

--- compiler.d
+++ compiler.d
@@ -4,10 +4,17 @@ import std.conv;
 import parser;
 import lexer;
 
-short sign_extend(int x, int bitcount) {
-  if ((x >> (bitcount - 1)) & 1) {
-    x |= (0xFFFF << bitcount);
-  }
+// short sign_extend(int x, int bitcount) {
+//   if ((x >> (bitcount - 1)) & 1) {
+//     x |= (0xFFFF << bitcount);
+//   }
+//   return cast(ushort)x;
+// }
+int sign_extend(int x, int bitcount) {
+  if (x < 0)
+    x |= (1 << (bitcount - 1));
+  else
+    x &= ~(1 << (bitcount - 1));
   return x;
 }
 
@@ -33,6 +40,14 @@ enum {
 class Compiler {
   private ushort[] code;
   private int pc;
+  
+  // ID for embed labels in macros and etc
+  private int lastUniId;
+
+  private string unify(string s) {
+    import std.format;
+    return format("%s_%X", s, lastUniId++);
+  }
 
   // BR, LD, LDI, LEA, ST, STI
   // replace 9 bits to PCoffset
@@ -43,36 +58,32 @@ class Compiler {
 
   private int[string] labels;
 
-  this(Parser p) {
-    parser = p;
-  }
+  private Node[string] macros;
 
-  ushort[] getCode() const {
+  const(ushort[]) getCode() const {
     return code;
   }
 
-  // ADD, AND
   private void addInstructionADD_AND(int instr, Reg r0, Reg r1, int imm5) {
     code ~= ((instr & 0xf) << 12) | ((r0 & 0x7) << 9) | ((r1 & 0x7) << 6) |
             (1 << 5) | (sign_extend(imm5, 5) & 0x1f);
     pc++;
   }
-  // ADD, AND
   private void addInstructionADD_AND(int instr, Reg r0, Reg r1, Reg r2) {
     code ~= ((instr & 0xf) << 12) | ((r0 & 0x7) << 9) | ((r1 & 0x7) << 6) | (r2 & 0x7);
     pc++;
   }
-  // BR
   private void addInstructionBR(string label, bool n, bool z, bool p) {
     // BR is 0000 opcode
+    bool nzp = !n && !z && !p;
+    n = n || nzp; z = z || nzp; p = p || nzp; // BR = BRnzp
     code ~= ((n & 1) << 11) | ((z & 1) << 10) | ((p & 1) << 9);
-    branches[pc++] = label;
+    pcoffsets9[pc++] = label;
   }
   private void addInstructionJSR(string label) {
     code ~= ((OP_JSR & 0xf) << 12) | (1 << 11);
     subroutines[pc++] = label;
   }
-  // JMP, JSRR
   private void addInstructionJMP_JSRR(int instr, Reg r0) {
     code ~= ((instr & 0xf) << 12) | ((r0 & 0x7) << 6);
     pc++;
@@ -132,32 +143,70 @@ class Compiler {
     error("Incorrect argument");
     assert(0);
   }
+  private string requireLabelArg(Node n, ref int counter) {
+    checkNodeChild(n, counter);
+    Node child = n.children[counter++];
+    if (child.kind == TokenType.ARG && child.value.type == LexValue.ValType.STRING) {
+      return child.value.asStr;
+    }
+    error("Incorrect argument");
+    assert(0);
+  }
 
   private void error(string msg, Node n = null) {
     import core.stdc.stdlib;
+    import std.stdio;
     import std.format;
     // if (!n)
-      writeln("Parser error: " ~ msg);
+      writeln("Compiler error: " ~ msg);
     // else
     // TODO
     exit(1);
   }
 
-  void compile(Node node) {
+  void compile(Node node, bool _macro = false) {
     int childCounter;
     switch (node.kind) {
-      case TokenType.ROOT:
+      // Instructions
+      case TokenType.ROOT: {
         foreach (n; node.children) {
           compile(n);
         }
+        if (_macro) break;
+        foreach (i, label; pcoffsets9) {
+          if (label in labels) {
+            code[i] |= sign_extend(labels[label] - i, 9) & 0x1ff;
+          }
+          else
+            error("Unknown label \"" ~ label ~ "\"");
+        }
+        foreach (i, label; subroutines) {
+          if (label in labels) {
+            code[i] |= sign_extend(labels[label] - i, 11) & 0x7ff;
+          }
+          else
+            error("Unknown label \"" ~ label ~ "\"");
+        }
         break;
-      case TokenType.ORG:
+      }
+      case TokenType.MACRO_DEF: {
+        if (!_macro)
+          macros[requireLabelArg(node, childCounter)] = node;
+        else {
+          goto case TokenType.ROOT;
+        }
+        break;
+      }
       case TokenType.INT:
-        code ~= requireIntArg(node, childCounter);
+      case TokenType.ORG: {
         pc++;
+        int i = requireIntArg(node, childCounter);
+        if (node.kind == TokenType.INT) code ~= cast(ushort)sign_extend(i, 16);
+        else code ~= cast(ushort) i;
         break;
+      }
       case TokenType.ADD:
-      case TokenType.AND:
+      case TokenType.AND: {
         int instr = OP_ADD ? node.kind == TokenType.ADD : OP_AND;
         Reg r0, r1;
         r0 = requireRegArg(node, childCounter);
@@ -169,9 +218,109 @@ class Compiler {
           addInstructionADD_AND(instr, r0, r1, requireRegArg(node, childCounter));
         else
           error("Incorrect argument. Register or decimal expected");
-        break;  
+        break;
+      }
+      case TokenType.BR: {
+        addInstructionBR(requireLabelArg(node, childCounter), node.options.n, node.options.z, node.options.p);
+        break;
+      }
+      case TokenType.JMP:
+      case TokenType.JSRR: {
+        int instr = OP_JMP ? node.kind == TokenType.JMP : OP_JSR;
+        addInstructionJMP_JSRR(instr, requireRegArg(node, childCounter));
+        break;
+      }
+      case TokenType.JSR: {
+        addInstructionJSR(requireLabelArg(node, childCounter));
+        break;
+      }
+      case TokenType.LD:
+      case TokenType.LDI:
+      case TokenType.LEA:
+      case TokenType.ST:
+      case TokenType.STI: {
+        // some shit to get instr
+        int instr;
+        switch (node.kind) {
+          case TokenType.LD: instr = OP_LD; break;
+          case TokenType.LDI: instr = OP_LDI; break;
+          case TokenType.LEA: instr = OP_LEA; break;
+          case TokenType.ST: instr = OP_ST; break;
+          case TokenType.STI: instr = OP_STI; break;
+          default: assert(0);
+        }
+        addInstructionLD_LDI_LEA_ST_STI(instr, requireRegArg(node, childCounter), requireLabelArg(node, childCounter));
+        break;
+      }
+      case TokenType.LDR:
+      case TokenType.STR: {
+        int instr = OP_LDR ? node.kind == TokenType.LDR : OP_STR;
+        addInstructionLDR_STR(instr, requireRegArg(node, childCounter), requireRegArg(node, childCounter), 
+                              requireIntArg(node, childCounter));
+        break;
+      }
+      case TokenType.NOT: {
+        addInstructionNOT(requireRegArg(node, childCounter), requireRegArg(node, childCounter));
+        break;
+      }
+      case TokenType.RET: {
+        addInstructionRET();
+        break;
+      }
+      case TokenType.TRAP: {
+        addInstructionTRAP(requireIntArg(node, childCounter));
+        break;
+      }
+      // Directives
+      case TokenType.BYTE: {
+        code ~= sign_extend(requireIntArg(node, childCounter), 8) & 0xff;
+        pc++;
+        break;
+      }
+      case TokenType.LONG: {
+        int i = requireIntArg(node, childCounter);
+        code ~= (i >> 15) & 0xffff;
+        code ~= i & 0xffff;
+        break;
+      }
+      case TokenType.BLKW: {
+        for (int i = requireIntArg(node, childCounter); i > 0; --i) {
+          code ~= 0;
+          pc++;
+        }
+        break;
+      }
+      case TokenType.ASCIZ:
+      case TokenType.ASCII: {
+        string s = requireLabelArg(node, childCounter);
+        foreach (c; s) {
+          code ~= cast(ushort) c;
+          pc++;
+        }
+        if (node.kind == TokenType.ASCIZ) {
+          code ~= 0;
+          pc++;
+        }
+        break;
+      }
+      // Other
+      case TokenType.ID: {
+        string id = node.value.asStr;
+        if (id in macros) {
+          compile(macros[id], true);
+        }
+        else {
+          // error("Unexpected identificator: " ~ id);
+        }
+        break;
+      }
+      case TokenType.LABEL: {
+        string label = node.value.asStr;
+        labels[label] = pc - 1; // magic number (There's something about offsetting and something else)
+        break;
+      }
       default:
-        error("Unexpected token");
+        // error("Unexpected token");
     }
   }
 }
